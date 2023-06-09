2aa5489c99db6a312c93043da8bb797930461755
ishrar776 <89441607+ishrar776@users.noreply.github.com>
2023-06-09 09:24:34 +0530

    1 file modified

--- src/Child.js
+++ src/Child.js
@@ -1,10 +1,41 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { FirstNmae } from './App';
 const ComB = () => {
+  const [input, setInput] = useState('');
+  const [todoList, AddTodoList] = useState([]);
   const fName = useContext(FirstNmae);
+  const addTodoList = (e) => {
+    e.preventDefault();
+    const id = todoList.length + 1;
+    AddTodoList((prev) => [
+      ...prev,
+      {
+        id: id,
+        task: input,
+      },
+    ]);
+    setInput('');
+  };
   return (
     <>
       <div>{fName}</div>
+      <div>
+        <input
+          type="text"
+          value={input}
+          name="txtName"
+          onInput={(e) => setInput(e.target.value)}
+        />
+        <input type="submit" onClick={addTodoList} />
+        <button onClick={addTodoList}>Add Item</button>
+      </div>
+      <div>
+        <ul>
+          {todoList.map((item) => {
+            <li id={item.id}>{item.task}</li>;
+          })}
+        </ul>
+      </div>
       {/* <FirstNmae.Consumer>
         {(fNmae1) => {
           return <>{fNmae1}</>;
